9f51f3a0b219f4d5d3efbaacdd929b0ff98e50cb
Alejandro Giraldo <manuel.giraldo.mt@correo.usa.edu.co>
2021-12-03 23:35:12 -0500

    Recorrido save finalizado, falta solucionar problemas de los query

--- src/main/java/com/hackaton/HackatonApplication.java
+++ src/main/java/com/hackaton/HackatonApplication.java
@@ -2,7 +2,9 @@ package com.hackaton;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages={"com.hackaton.modelo"})
 @SpringBootApplication
 public class HackatonApplication {
 
--- src/main/java/com/hackaton/controlador/ControladorModelo.java
+++ src/main/java/com/hackaton/controlador/ControladorModelo.java
@@ -1,23 +1,11 @@
 package com.hackaton.controlador;
 
-import com.hackaton.controlador.servicio.ServicioModelo;
-import java.util.List;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 /**
  *
  * @author IngSB
  */
-@RestController
-@RequestMapping("/api/user")
 public class ControladorModelo {
-    @Autowired
-    private ServicioModelo servicioModelo;
-    
-        @GetMapping("/all")
-    public List<User> getAll() {
-        return servicioModelo.getAll();
-    }
+
     
 }
